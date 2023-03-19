90566d35843fbdaf894a256e2d6df27c4df7ca49
Younes Brahimi <43382860+younesbram@users.noreply.github.com>
2023-03-19 15:06:15 -0400

    Changes1

--- query_data.py
+++ query_data.py
@@ -2,11 +2,11 @@
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
-from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
-                                                     QA_PROMPT)
+from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.chains.question_answering.stuff_prompt import PROMPT_SELECTOR
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores.base import VectorStore
 
 
@@ -26,12 +26,12 @@ def get_chain(
         question_manager.add_handler(tracer)
         stream_manager.add_handler(tracer)
 
-    question_gen_llm = OpenAI(
+    question_gen_llm = ChatOpenAI(
         temperature=0,
         verbose=True,
         callback_manager=question_manager,
     )
-    streaming_llm = OpenAI(
+    streaming_llm = ChatOpenAI(
         streaming=True,
         callback_manager=stream_manager,
         verbose=True,
@@ -41,8 +41,9 @@ def get_chain(
     question_generator = LLMChain(
         llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
     )
+    qa_prompt = PROMPT_SELECTOR.get_prompt(streaming_llm)
     doc_chain = load_qa_chain(
-        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
+        streaming_llm, chain_type="stuff", prompt=qa_prompt, callback_manager=manager
     )
 
     qa = ChatVectorDBChain(
